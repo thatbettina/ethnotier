a41146ff506e8dd4ecdb9f887253cf3bd9c7559f
Thomas Tursics <thomas@tursics.de>
2014-05-09 22:52:10 +0200

    remove drag events and add mouse and touch events

--- www/js/instrument.js
+++ www/js/instrument.js
@@ -12,35 +12,77 @@ function Instrument() {
 Instrument.prototype = {
 	// -------------------------------------------------------------------------
 	create: function() {
-		$( '#mapContainer').append( '<div id="instrument" draggable="true">instrument</div><audio id="GoldenEagle" src="http://species-id.net/o/media/e/e9/Aquila_chrysaetos_TSA-short.mp3" preload="auto"></audio>');
+		$( '#mapContainer').append( '<div id="instrument">instrument</div><audio id="GoldenEagle" src="http://species-id.net/o/media/e/e9/Aquila_chrysaetos_TSA-short.mp3" preload="auto"></audio>');
 		this.obj = $( '#instrument');
 
-		this.obj.on( 'dragstart', function( event) {
-			event.originalEvent.dataTransfer.setData( 'text/html', 'GoldenEagle');
-			this.style.opacity = '0.6';
-//			event.dataTransfer.effectAllowed = 'move';
-		});
+		var obj = this;
 
-		// to do: remove drag events and switch to touch and mouse events
-		// (iPad have no drag events)
+		this.obj.on( 'selectstart', function() {
+			return false;
+		});
+		this.obj.on( 'touchstart mousedown', function( event) {
+			event.preventDefault();
 
-		this.obj.on( 'touchstart', function( event) {
-			event.preventDefault();
-			this.style.opacity = '0.6';
-		});
-		this.obj.on( 'touchmove', function( event) {
-			event.preventDefault();
-			var touch = event.originalEvent.touches[0];
-//			alert( touch.pageX + " - " + touch.pageY);
-		});
-		this.obj.on( 'touchend', function( event) {
-			if( event.stopPropagation) {
-				event.stopPropagation();
+			if(( typeof event.originalEvent.touches !== 'undefined') && (event.originalEvent.touches.length > 0)) {
+				event.pageX = event.originalEvent.touches[0].pageX;
+				event.pageY = event.originalEvent.touches[0].pageY;
 			}
-			this.style.opacity = '1';
-			var sound = new Sound;
-			sound.play( "GoldenEagle");
+
+			obj.dragMousePos = { x: event.pageX, y: event.pageY };
+			obj.dragMouseDiff = { x: 0, y: 0 };
+
+			$( document).on( 'touchmove mousemove', eventDragMoveEvent);
+			$( document).on( 'touchend mouseup mouseleave', eventDragEndFunc);
+
+			obj.dragStartFunc.call( obj, event);
+
+			function eventDragMoveEvent( event)
+			{
+				event.preventDefault();
+
+				if(( typeof event.originalEvent.touches !== 'undefined') && (event.originalEvent.touches.length > 0)) {
+					event.pageX = event.originalEvent.touches[0].pageX;
+					event.pageY = event.originalEvent.touches[0].pageY;
+				}
+
+				obj.dragMouseDiff.x = event.pageX - obj.dragMousePos.x;
+				obj.dragMouseDiff.y = event.pageY - obj.dragMousePos.y;
+
+				obj.dragMoveFunc.call( obj, event);
+			}
+
+			function eventDragEndFunc( event)
+			{
+				event.stopPropagation();
+
+				$( document).off( 'touchmove mousemove', eventDragMoveEvent);
+				$( document).off( 'touchend mouseup mouseleave', eventDragEndFunc);
+
+				if(( typeof event.originalEvent.touches !== 'undefined') && (event.originalEvent.touches.length > 0)) {
+					event.pageX = event.originalEvent.touches[0].pageX;
+					event.pageY = event.originalEvent.touches[0].pageY;
+				}
+
+				obj.dragEndFunc.call( obj, event);
+			}
 		});
+	},
+	// -------------------------------------------------------------------------
+	dragStartFunc: function( event) {
+		this.draggingStart = $( this.obj).position();
+
+		$( this.obj).css({ opacity: '0.8', position: 'absolute', left: this.draggingStart.left, top: this.draggingStart.top });
+	},
+	// -------------------------------------------------------------------------
+	dragMoveFunc: function( event) {
+		$( this.obj).css({ left: this.draggingStart.left + this.dragMouseDiff.x, top: this.draggingStart.top  + this.dragMouseDiff.y});
+	},
+	// -------------------------------------------------------------------------
+	dragEndFunc: function( event) {
+		$( this.obj).css({ opacity: '1'});
+
+		var sound = new Sound;
+		sound.play( 'GoldenEagle');
 	},
 	// -------------------------------------------------------------------------
 }
--- www/js/orchestra.js
+++ www/js/orchestra.js
@@ -17,33 +17,24 @@ Orchestra.prototype = {
 		this.obj = $( '#orchestra');
 		var that = this;
 
-		this.obj.on( 'dragenter', function( event) {
-			this.classList.add( 'over');
-		});
-		this.obj.on( 'dragleave', function( event) {
-			this.classList.remove( 'over');
-		});
-		this.obj.on( 'dragend', function( event) {
-			this.classList.remove( 'over');
-		});
-		this.obj.on( 'dragover', function( event) {
-			if( event.preventDefault) {
-				event.preventDefault();
-			}
-
-//			event.dataTransfer.dropEffect = 'move';
-
-			return false;
-		});
-		this.obj.on( 'drop', function( event) {
-			if( event.stopPropagation) {
-				event.stopPropagation();
-			}
-			this.classList.remove( 'over');
-			this.innerHTML = event.originalEvent.dataTransfer.getData( 'text/html');
-			that.sound.play( event.originalEvent.dataTransfer.getData( 'text/html'));
-			return false;
-		});
+//		this.obj.on( 'dragenter', function( event) {
+//			this.classList.add( 'over');
+//		});
+//		this.obj.on( 'dragleave', function( event) {
+//			this.classList.remove( 'over');
+//		});
+//		this.obj.on( 'dragend', function( event) {
+//			this.classList.remove( 'over');
+//		});
+//		this.obj.on( 'drop', function( event) {
+//			if( event.stopPropagation) {
+//				event.stopPropagation();
+//			}
+//			this.classList.remove( 'over');
+//			this.innerHTML = event.originalEvent.dataTransfer.getData( 'text/html');
+//			that.sound.play( event.originalEvent.dataTransfer.getData( 'text/html'));
+//			return false;
+//		});
 	},
 	// -------------------------------------------------------------------------
 }
