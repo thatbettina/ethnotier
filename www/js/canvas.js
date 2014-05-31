037e71730bdda0a02fbe1c61947c162aa8444e86
Thomas Tursics <thomas@tursics.de>
2014-05-31 02:53:36 +0200

    added the trees

--- www/js/canvas.js
+++ www/js/canvas.js
@@ -23,6 +23,7 @@ Canvas.prototype = {
 			var factorX = winWidth / imgWidth;
 			var factorY = winHeight / imgHeight;
 			var factor = factorX > factorY ? factorX : factorY;
+			var shift = factorX > factorY ? 0 : 750 * (factorX - factorY);
 
 			imgWidth *= factor;
 			imgHeight *= factor;
@@ -30,6 +31,30 @@ Canvas.prototype = {
 			$( '#imgBackground').css({
 				top: parseInt( centerY - imgHeight / 2) + 'px',
 				left: parseInt( centerX - imgWidth / 2) + 'px',
+				width: parseInt( imgWidth) + 'px',
+				height: parseInt( imgHeight) + 'px',
+			});
+			$( '#imgTreeleft').css({
+				top: parseInt( centerY - imgHeight / 2) + 'px',
+				left: parseInt( centerX - imgWidth / 2 - shift) + 'px',
+				width: parseInt( imgWidth) + 'px',
+				height: parseInt( imgHeight) + 'px',
+			});
+			$( '#imgTreeright').css({
+				top: parseInt( centerY - imgHeight / 2) + 'px',
+				left: parseInt( centerX - imgWidth / 2 + shift) + 'px',
+				width: parseInt( imgWidth) + 'px',
+				height: parseInt( imgHeight) + 'px',
+			});
+			$( '#imgFlagleft').css({
+				top: parseInt( centerY - imgHeight / 2) + 'px',
+				left: parseInt( centerX - imgWidth / 2 - shift) + 'px',
+				width: parseInt( imgWidth) + 'px',
+				height: parseInt( imgHeight) + 'px',
+			});
+			$( '#imgFlagright').css({
+				top: parseInt( centerY - imgHeight / 2) + 'px',
+				left: parseInt( centerX - imgWidth / 2 + shift) + 'px',
 				width: parseInt( imgWidth) + 'px',
 				height: parseInt( imgHeight) + 'px',
 			});
@@ -44,6 +69,16 @@ Canvas.prototype = {
 		preload.addCSS( '#imgBackground', 'position:absolute;z-index:0;width:0;height:0;');
 		preload.addImage( 'imgBackground', 'art/background.svg');
 
+		preload.addCSS( '#imgTreeleft', 'position:absolute;z-index:1;width:0;height:0;');
+		preload.addCSS( '#imgTreeright', 'position:absolute;z-index:1;width:0;height:0;');
+		preload.addImage( 'imgTreeleft', 'art/treeleft.svg');
+		preload.addImage( 'imgTreeright', 'art/treeright.svg');
+
+		preload.addCSS( '#imgFlagleft', 'position:absolute;z-index:3;width:0;height:0;');
+		preload.addCSS( '#imgFlagright', 'position:absolute;z-index:3;width:0;height:0;');
+		preload.addImage( 'imgFlagleft', 'art/flagleft.svg');
+		preload.addImage( 'imgFlagright', 'art/flagright.svg');
+
 		preload.wait( function() {
 			callback();
 		});
@@ -51,7 +86,12 @@ Canvas.prototype = {
 	// -------------------------------------------------------------------------
 	wakeUp: function() {
 		this.onResize();
+
 		$( '#imgBackground').removeClass( 'hidden');
+		$( '#imgTreeleft').removeClass( 'hidden');
+		$( '#imgTreeright').removeClass( 'hidden');
+		$( '#imgFlagleft').removeClass( 'hidden');
+		$( '#imgFlagright').removeClass( 'hidden');
 	},
 	// -------------------------------------------------------------------------
 }
