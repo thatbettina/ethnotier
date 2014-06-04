cb2b63324b7332eaccfbfc822f865c923f66ad57
Thomas Tursics <thomas@tursics.de>
2014-06-05 00:23:36 +0200

    added audio files from Ethonogical Museum Berlin

--- www/js/cabinet.js
+++ www/js/cabinet.js
@@ -19,7 +19,7 @@ Cabinet.prototype = {
 	// -------------------------------------------------------------------------
 	initInstruments: function() {
 		this.instruments = [
-		// source: http://offene-naturfuehrer.de/web/Open_Source_Tierstimmen
+		// source: CC BY-SA http://offene-naturfuehrer.de/web/Open_Source_Tierstimmen
 		{name:'toad',    src:'http://species-id.net/o/media/7/76/Bufo_calamita_TSA-medium.mp3'},
 		{name:'swift',   src:'http://species-id.net/o/media/b/ba/Apus_apus_TSA-medium.mp3'},
 		{name:'deer',    src:'http://species-id.net/o/media/3/33/Dama_dama_TSA-medium.mp3'},
@@ -28,6 +28,23 @@ Cabinet.prototype = {
 		{name:'mallard', src:'http://species-id.net/o/media/b/b0/Anas_platyrhynchos_TSA-medium.mp3'},
 		{name:'owl',     src:'http://species-id.net/o/media/6/61/Bubo_bubo_TSA-medium.mp3'},
 		{name:'wolf',    src:'http://species-id.net/o/media/4/4f/Canis_lupus_TSA-medium.mp3'},
+/*		{name:'toad',    src:'media/Bufo_calamita_TSA-medium.mp3'},
+		{name:'swift',   src:'media/Apus_apus_TSA-medium.mp3'},
+		{name:'deer',    src:'media/Dama_dama_TSA-medium.mp3'},
+		{name:'eagle',   src:'media/Aquila_chrysaetos_TSA-short.mp3'},
+		{name:'goose',   src:'media/Branta_canadensis_TSA-medium.mp3'},
+		{name:'mallard', src:'media/Anas_platyrhynchos_TSA-medium.mp3'},
+		{name:'owl',     src:'media/Bubo_bubo_TSA-medium.mp3'},
+		{name:'wolf',    src:'media/Canis_lupus_TSA-medium.mp3'},*/
+		// source: CC BY https://commons.wikimedia.org/wiki/Category:Audiodateien_des_Ethnologischen_Museums
+		{name:'I_C_1479',    src:'https://upload.wikimedia.org/wikipedia/commons/7/7b/I_C_1479_g_x.ogg'},
+		{name:'I_C_38635',   src:'https://upload.wikimedia.org/wikipedia/commons/d/da/I_C_38635_b_x.ogg'},
+		{name:'III_C_30616', src:'https://upload.wikimedia.org/wikipedia/commons/3/3c/III_C_30616_x.ogg'},
+		{name:'VII_c_823',   src:'https://upload.wikimedia.org/wikipedia/commons/3/33/VII_c_823_-B_x.ogg'},
+/*		{name:'I_C_1479',    src:'media/I_C_1479_g_x.ogg'},
+		{name:'I_C_38635',   src:'media/I_C_38635_b_x.ogg'},
+		{name:'III_C_30616', src:'media/III_C_30616_x.ogg'},
+		{name:'VII_c_823',   src:'media/VII_c_823_-B_x.ogg'},*/
 		];
 	},
 	// -------------------------------------------------------------------------
